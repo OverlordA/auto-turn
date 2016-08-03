b0b440e10e7728315a01b3522b4c1b894add3b1a
OverlordA <anton93_16@mail.ru>
2016-08-03 14:23:38 +0300

    add to data base work

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ var url = 'mongodb://192.168.2.108:27017/auto_turn';
 var ObjectId = MongoDriver.ObjectId;
 var formidable = require("formidable");
 var count1 = 0; 
+var filenamedb =0; 
+
 
 MongoClient.connect(url, function(err, db) { 
 	
@@ -106,26 +108,61 @@ if(req.body.reset=='true'){
 //сторінка загрузки img
 app.post('/renderaddcar', function(req, res) {
 
-res.render('addcar', {});
+res.render('addcar', {filenamedb:filenamedb});
 
 
 });
 
 app.post('/upload', function(req, res){
-
 var form = new formidable.IncomingForm();
 form.parse(req);
 
+
 form.on('fileBegin', function (name, file){
         file.path = __dirname + '/public/images/car/' + file.name;
+      
     });
 
  form.on('file', function (name, file){
         console.log('Uploaded ' + file.name);
+filenamedb = 'images/car/'+file.name; 
+res.render('addcar',{filenamedb:filenamedb});
     });
-
-
 });
+
+
+
+app.post('/addcar', function(req, res){
+
+console.log(req.body.brand);
+console.log(req.body.model);
+
+console.log(filenamedb);
+
+var insertDocuments = function(db, callback) {
+  			// Get the documents collection 
+  			var collection = db.collection('auto');
+  			// Insert some documents 
+  			collection.insertOne({model:(req.body.model),marka:(req.body.brand),urlimg:(filenamedb),count:[]}, function(err, result) {
+    			assert.equal(err, null);
+    			assert.equal(1, result.result.n);
+   				 assert.equal(1, result.ops.length);
+   				 console.log("Inserted 1 documents into the document collection");
+
+   				 callback(result);
+  			});
+		}
+  
+  
+  		insertDocuments(db, function() {
+
+	
+		}); 
+
+res.render('addcar', {filenamedb:filenamedb});
+});
+
+
 
 
 //disconect db	
